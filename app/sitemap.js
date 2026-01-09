b9dadeb0de9ab49b2bde6200bb36e7e2600d1a8b
Adebayo-jzs <adedejiadebayo732@gmail.com>
2026-01-09 20:44:29 +0100

    search engine optimisation

---
+++ app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  const baseUrl = 'https://textcognito.com'
+  
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    // {
+    //   url: `${baseUrl}/about`,
+    //   lastModified: new Date(),
+    //   changeFrequency: 'monthly',
+    //   priority: 0.8,
+    // },
+    {
+      url: `${baseUrl}/auth/login`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+  ]
+}
