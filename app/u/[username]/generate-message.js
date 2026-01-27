31aa852878cf5491b73cd41620c76cca1187eda9
Adebayo <adedejiadebayo732@gmail.com>
2026-01-27 17:57:52 +0100

    integrated openai api

--- app/u/[username]/generate-message.js
+++ app/u/[username]/generate-message.js
@@ -1,18 +1,33 @@
 "use server"
 import { GoogleGenAI } from "@google/genai";
+import OpenAI from "openai";
 
 export async function GenerateMessage(){
-    const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+    const prompt = "Generate a short, funny, and friendly anonymous message (under 15 words) that I can send to a friend. Return ONLY the text.";
+    const gemini = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+    const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
     try{
-        const response = await ai.models.generateContent({
+        const response = await gemini.models.generateContent({
           model: "gemini-2.5-flash",
-          contents: "Generate a short, funny, and friendly anonymous message (under 15 words) that I can send to a friend. Return ONLY the text.",
+          contents: prompt,
         });
         return response.text;
         console.log(response.text);
-    } catch(error) {
-        console.error("Gemini erroe:", error)
-        return "You're awesome!";
+        console.log("Success with Gemini");
+    } catch(geminiError){
+        try{
+            const response = await client.responses.create({
+                model: "gpt-5-nano",
+                input: prompt
+            });
+            return response.output_text;
+            console.log(response.output_text);
+            console.log("Success with OpenAI");
+        } catch (openaiError) {
+            // Final Fallback: Hardcoded message
+            console.error("OpenAI error:", openaiError);
+            return "You're awesome!";
+        }
+    
     }
-  
 }
