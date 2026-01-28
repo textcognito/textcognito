04f375d3037387f48b7d39af3245bfd3ebce8174
Adebayo <adedejiadebayo732@gmail.com>
2026-01-28 09:10:14 +0100

    removed unused functions

--- app/u/[username]/generate-message.js
+++ app/u/[username]/generate-message.js
@@ -3,7 +3,7 @@ import { GoogleGenAI } from "@google/genai";
 import OpenAI from "openai";
 
 export async function GenerateMessage(){
-    const prompt = "Generate a short, funny, and friendly anonymous message (under 15 words) that I can send to a friend. Return ONLY the text.";
+    const prompt = "Generate a short, funny, and friendly anonymous message (under 15 words) that I can send to a friend. Return ONLY the text. ";
     const gemini = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
     const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
     try{
@@ -11,20 +11,15 @@ export async function GenerateMessage(){
           model: "gemini-2.5-flash",
           contents: prompt,
         });
-        return response.text;
-        console.log(response.text);
-        console.log("Success with Gemini");
+        return response.text; 
     } catch(geminiError){
         try{
             const response = await client.responses.create({
                 model: "gpt-5-nano",
                 input: prompt
             });
-            return response.output_text;
-            console.log(response.output_text);
-            console.log("Success with OpenAI");
+            return response.output_text; 
         } catch (openaiError) {
-            // Final Fallback: Hardcoded message
             console.error("OpenAI error:", openaiError);
             return "You're awesome!";
         }
