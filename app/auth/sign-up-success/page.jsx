92d228a9f25e2278ce1c235c7a5d2e174c855593
Adebayo <adedejiadebayo732@gmail.com>
2026-02-21 22:16:42 +0100

    updated auth components

--- app/auth/sign-up-success/page.jsx
+++ app/auth/sign-up-success/page.jsx
@@ -11,7 +11,7 @@ export default function Page() {
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <div className="flex flex-col gap-6">
-          <Card>
+          <Card className="bg-transparent border-transparent">
             <CardHeader>
               <CardTitle className="text-2xl">Thank you for signing up!</CardTitle>
               <CardDescription>Check your email to confirm</CardDescription>
